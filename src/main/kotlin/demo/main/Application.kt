ec595a6ac62f2a1273b7d4524baf360cc95881ad
Lelton Borges <leltondev@gmail.com>
2022-05-05 13:32:34 -0300

    feat: declaring variables

--- src/main/kotlin/demo/main/Application.kt
+++ src/main/kotlin/demo/main/Application.kt
@@ -1,5 +1,21 @@
 package demo.main
 
 fun main(){
-    print("Hello");
+    println("Hello");
+    val titular = "Lelton";
+    var nome: String = "José";
+
+    nome = "Bob";
+    val nomeSobrenome = lelton(nome);
+
+    val accountNumber = 10023;
+    var balance: Double = 100.0;
+    balance += 1;
+
+    println("Titular: $titular")
+    println("Nome completo: $nomeSobrenome")
+}
+
+fun lelton(nome:String): String{
+    return "$nome Borges";
 }
