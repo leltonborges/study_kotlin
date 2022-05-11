1803f886cdc4384f5fab3cf7f6d2de205afd1db4
Lelton Borges <leltondev@gmail.com>
2022-05-11 13:44:52 -0300

    feat: attribute null

--- src/main/kotlin/demo/main/list/ApplicationListaLivrosComNulo.kt
+++ src/main/kotlin/demo/main/list/ApplicationListaLivrosComNulo.kt
@@ -4,4 +4,17 @@ fun main() {
     val livrosComNulos = getLivrosComNulos()
 
     livrosComNulos.imprime()
+
+    val livros = getLivros()
+    livros.groupBy { it.editora }
+        .forEach { (editora: String?, livros: List<Livro>)
+            -> println("$editora: ${livros.joinToString { it.titulo }}")
+        }
+
+    println()
+
+    livros.groupBy { it.editora ?: "Sem editora" }
+        .forEach{(editora, livros)
+            -> println("$editora: ${livros.joinToString { it.titulo }}")
+        }
 }
