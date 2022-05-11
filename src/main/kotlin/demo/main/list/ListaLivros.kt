5f94bf9e68f9936a78c98ffac490b4e274b48c3c
Lelton Borges <leltondev@gmail.com>
2022-05-11 11:49:50 -0300

    feat: list map, filter, sortedBy

--- src/main/kotlin/demo/main/list/ListaLivros.kt
+++ src/main/kotlin/demo/main/list/ListaLivros.kt
@@ -1,34 +1,7 @@
 package demo.main.list
 
 fun main() {
-    val livro1 = Livro(
-        titulo = "Grande Sertão: Veredas",
-        autor = "João Guimarães Rosa",
-        anoPublicacao = 1956
-    )
-
-    val livro2 = Livro(
-        titulo = "Minha vida de menina",
-        autor = "Helena Morley",
-        anoPublicacao = 1942,
-        editora = "Editora A"
-    )
-
-    val livro3 = Livro(
-        titulo = "Memórias Póstumas de Brás Cubas",
-        autor = "Machado de Assis",
-        anoPublicacao = 1881
-    )
-
-    val livro4 = Livro(
-        titulo = "Iracema",
-        autor = "José de Alencar",
-        anoPublicacao = 1865,
-        editora = "Editora B"
-    )
-
-    val livros: MutableList<Livro> = mutableListOf(livro1, livro2, livro3, livro4)
-    livros.add(Livro("Sagarana", "João Guimarẽs Rosa", 1946))
+    val livros = getLivros();
 
     println()
     livros.imprime()
@@ -45,9 +18,18 @@ fun main() {
     val livrosOrdenadoPorAutor = livros.sortedBy { it.autor }
     livrosOrdenadoPorAutor.imprime()
 
-}
+    println("\nListas filter")
+    println("==============")
 
-fun List<Livro>.imprime(){
-    val joinToString = this.joinToString(separator = "\n") { l: Livro -> "- ${l.titulo} de ${l.autor}" }
-    println(joinToString)
+    println()
+    livros.filter { it.autor.equals("José de Alencar") }
+        .imprime()
+
+    println("\nStartsWith")
+    livros.filter { it.titulo.startsWith("Pix") }.imprime()
+
+    println("\nMap filter contains")
+    livros.filter { it.titulo.contains("Test", true) }
+        .map { it.titulo }
+        .forEach{ println(it) }
 }
---
+++ src/main/kotlin/demo/main/list/LivroListas.kt
@@ -0,0 +1,69 @@
+package demo.main.list
+
+fun getLivros(): List<Livro> {
+    val livro1 = Livro(
+        titulo = "Grande Sertão: Veredas",
+        autor = "João Guimarães Rosa",
+        anoPublicacao = 1956
+    )
+
+    val livro2 = Livro(
+        titulo = "Minha vida de menina",
+        autor = "Helena Morley",
+        anoPublicacao = 1942,
+        editora = "Editora A"
+    )
+
+    val livro3 = Livro(
+        titulo = "Memórias Póstumas de Brás Cubas",
+        autor = "Machado de Assis",
+        anoPublicacao = 1881
+    )
+
+    val livro4 = Livro(
+        titulo = "Iracema",
+        autor = "José de Alencar",
+        anoPublicacao = 1865,
+        editora = "Editora B"
+    )
+
+    val livro5 = Livro(
+        titulo = "Test A",
+        autor = "José de Alencar",
+        anoPublicacao = 1865,
+        editora = "Editora B"
+    )
+
+    val livro6 = Livro(
+        titulo = "Pix TED Test",
+        autor = "José de Alencar",
+        anoPublicacao = 1865,
+        editora = "Editora B"
+    )
+
+    val livro7 = Livro(
+        titulo = "Test Abc",
+        autor = "José de Alencar",
+        anoPublicacao = 1865,
+        editora = "Editora B"
+    )
+
+    val livro8 = Livro(
+        titulo = "Pix TED TEST",
+        autor = "José de Alencar",
+        anoPublicacao = 1865,
+        editora = "Editora B"
+    )
+
+    val livros: MutableList<Livro> =
+        mutableListOf(livro1, livro2, livro3, livro4, livro5, livro6, livro7, livro8, livro1, livro2, livro3, livro4, livro5, livro6, livro7, livro8, livro1, livro2, livro3, livro4, livro5, livro6, livro7, livro8)
+    livros.add(Livro("Sagarana", "João Guimarẽs Rosa", 1946))
+
+    return livros;
+}
+
+
+fun List<Livro>.imprime() {
+    val joinToString = this.joinToString(separator = "\n") { l: Livro -> "- ${l.titulo} de ${l.autor}" }
+    println(joinToString)
+}
