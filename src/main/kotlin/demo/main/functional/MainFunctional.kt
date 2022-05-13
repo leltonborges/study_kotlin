1175b46266ccea91a4298fd9a4d1259afd4d222d
Lelton Borges <leltondev@gmail.com>
2022-05-13 17:12:01 -0300

    feat: Anonymous function and lambda expressions

---
+++ src/main/kotlin/demo/main/functional/MainFunctional.kt
@@ -0,0 +1,33 @@
+package demo.main.functional
+
+fun main() {
+    val minhaFuncao: () -> Unit = ::test
+
+    println(minhaFuncao())
+
+    val minhaClasse: (Int, Int) -> Unit = Teste()
+    println(minhaClasse(2,3))
+
+    /////////////////////////////////
+
+    val myFunctionLambda1: () -> Unit = { // Lambda expression
+        println("Executa com um lambda")
+    }
+
+    println(myFunctionLambda1) // só mostra a funcao
+    println(myFunctionLambda1()) // Execulta
+
+    ////////////////
+    println()
+
+    val myFunctionLambda2: () -> Unit = fun() { // Função anonimas
+        println("Executa com um lambda")
+    }
+
+    println(myFunctionLambda2) // só mostra a funcao
+    println(myFunctionLambda2()) // Execulta
+}
+
+fun test(){
+    println("Executa test")
+}
---
+++ src/main/kotlin/demo/main/functional/Teste.kt
@@ -0,0 +1,8 @@
+package demo.main.functional
+
+class Teste: (Int, Int) -> Unit {
+    override fun invoke(p1: Int, p2: Int) {
+        println("n1: $p1, n2: $p2")
+    }
+
+}
