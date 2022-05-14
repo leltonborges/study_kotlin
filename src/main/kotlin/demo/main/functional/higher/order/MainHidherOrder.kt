59aedee0978f85ed29cda173d08ff9e6d3254bc2
Lelton Borges <leltondev@gmail.com>
2022-05-13 22:44:26 -0300

    feat: higher order functions @1

--- src/main/kotlin/demo/main/functional/higher/order/MainHidherOrder.kt
+++ src/main/kotlin/demo/main/functional/higher/order/MainHidherOrder.kt
@@ -1,2 +1,22 @@
 package demo.main.functional.higher.order
 
+fun main() {
+    test { }
+    "".let(::testRecebeString)
+
+    test2({}, 1)
+    test3(1, {})
+    test4{}
+}
+
+fun test(bloco: () -> Unit) {}
+fun test2(bloco: () -> Unit, test: Int) {}
+fun test3(num: Int, bloco: () -> Unit) {}
+fun test4(bloco: () -> Unit): () -> Unit {
+    return {
+        println("")
+    }
+}
+
+fun testRecebeString(value: String) {}
+
